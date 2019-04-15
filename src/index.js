b940f4b9f2d635e7f2543f8c561ecde1bbc049fd
Zak Stephens <zak.j.stephens@gmail.com>
2019-04-16 00:50:59 +1000

    Add more skeleton.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('Hello, Webpack!');
+
+
+
+
+// TODO: Require, load game states, initial setup, etc. here.
+
+function getSnapshot() {
+  // TODO
+}
+
+document.addEventListener('DOMContentLoaded', event => {
+
+  
+
+  const context = document
+    .getElementById('viewport')
+    .getContext('2d');
+
+  // TODO: Fire off event loop, calling functions on current game state.
+
+  setInterval(() => {
+    
+  }, 1000/fps);
+
+
+  const snapshots = [];
+
+  setInterval(() => {
+    snapshots.push(getSnapshot());
+  }, 1000);
+
+});
